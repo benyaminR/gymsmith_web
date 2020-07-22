a5118a1ca3e88b150c606aae955c7dabd21d66bf
Benyamin <b.radmard77@gmai.com>
2020-07-22 08:59:51 +0200

    +Added Provider, Responsive UI and UI Builder +Added Color Swatch

---
+++ lib/Colors/color_swatches.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/cupertino.dart';
+const Color Grey_Background = Color(0xFFF8F8F8);
+const Color White = Color(0xFFFFFFFF);
+const Color Grey_Light = Color(0xFF8F8F8F);
+const Color Grey_Dark = Color(0xFF606060);
+const Color Black = Color(0xFF000000);
+const Color Orange = Color(0xFFFF8E18);
+
+
