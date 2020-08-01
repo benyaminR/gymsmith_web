98d7811999b5cf68862f41ed6c4d6c631e12ef6f
Benyamin <b.radmard77@gmai.com>
2020-08-01 13:42:32 +0200

    +Refactor Divider +added Responsiveness to [Divider]

---
+++ lib/core/common_widgets/divider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+class CommonDivider extends StatelessWidget {
+  const CommonDivider({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Container(
+        decoration: BoxDecoration(color: Colors.grey[600], boxShadow: [
+          BoxShadow(offset: Offset(0, 1), blurRadius: 2, color: Colors.black26)
+        ]),
+        height: 0.5,
+        width: sizingInformation.screenSize.width/2.25,
+      ),
+    );
+  }
+}
--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gymsmith_web/core/common_widgets/bottom_nav_bar.dart';
 import 'package:gymsmith_web/core/common_widgets/common_drawer.dart';
+import 'package:gymsmith_web/core/common_widgets/divider.dart';
 
 import 'package:gymsmith_web/home_page/home_page_slide_show_intro.dart';
 import 'package:gymsmith_web/core/common_widgets/app_bar.dart';
@@ -17,63 +18,48 @@ class HomePage extends StatelessWidget {
         appBar: PreferredSize(
             child: CommonAppBar(),
             preferredSize: Size(MediaQuery.of(context).size.width, 70)),
-        drawer:  ! sizingInformation.isDesktop ? CommonDrawer() : null,
+        drawer: !sizingInformation.isDesktop ? CommonDrawer() : null,
         body: SingleChildScrollView(
           child: Container(
-            height: 3979,
+            //height: 3979,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 SizedBox(
                   height: 38,
                 ),
-
-                HomePageSlideShowIntro(duration: Duration(seconds: 1), delay: Duration(seconds: 5),sizingInformation: sizingInformation,),
-
+                HomePageSlideShowIntro(
+                  duration: Duration(seconds: 1),
+                  delay: Duration(seconds: 5),
+                  sizingInformation: sizingInformation,
+                ),
                 SizedBox(
                   height: 119,
                 ),
                 DealsOfTheDayWidget(),
-
-                SizedBox(height: 45.5,),
-
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[600],
-                      boxShadow: [
-                        BoxShadow(
-                            offset: Offset(0,1),
-                            blurRadius: 2,
-                            color: Colors.black26
-                        )
-                      ]
-                  ),
-                  height: 0.5,
-                  width: 834,
+                SizedBox(
+                  height: 45.5,
                 ),
 
-                SizedBox(height: 21.5,),
+                CommonDivider(),
 
-                HomePageBottomSlideShow(duration: Duration(seconds: 1), delay: Duration(seconds: 5),),
-
-                SizedBox(height: 29.5,),
-
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[600],
-                      boxShadow: [
-                        BoxShadow(
-                            offset: Offset(0,1),
-                            blurRadius: 2,
-                            color: Colors.black26
-                        )
-                      ]
-                  ),
-                  height: 0.5,
-                  width: 834,
+                SizedBox(
+                  height: 21.5,
+                ),
+                HomePageBottomSlideShow(
+                  duration: Duration(seconds: 1),
+                  delay: Duration(seconds: 5),
                 ),
 
-                SizedBox(height:235.5,),
+                SizedBox(
+                  height: 29.5,
+                ),
+
+                CommonDivider(),
+
+                SizedBox(
+                  height: 235.5,
+                ),
 
                 BottomNavBar(),
               ],
